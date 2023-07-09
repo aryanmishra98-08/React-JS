f5d379d97bdd832e7cedb6ce3ade877e997c0b53
Aryan <aryanmishra98.08@gmail.com>
2023-07-09 17:01:53 +0530

    formatting the code

--- Java Script Recall/assets/scripts/app.js
+++ Java Script Recall/assets/scripts/app.js
@@ -1,6 +1,6 @@
 console.log("Import a variable")
-import { project_manager } from "./utils.js";
-console.log(project_manager);
+import { projectManager } from "./utils.js";
+console.log(projectManager);
 
 console.log("Import default variable")
 import project from "./utils.js"
@@ -9,13 +9,13 @@ console.log(project);
 //In order to export multiple variables at once we need to create an object of the utils file 
 console.log("Import all variables at once")
 import * as utils from "./utils.js";
-console.log(utils.project_manager)
+console.log(utils.projectManager)
 console.log(utils.default)
-console.log(utils.project_technology)
+console.log(utils.projectTechnology)
 
 //Using 'as' keyword for assigning an alias
 console.log("Using 'as' keyword")
-import { project_manager as name} from "./utils.js";
+import { projectManager as name} from "./utils.js";
 console.log(name);
 
 
--- Java Script Recall/assets/scripts/utils.js
+++ Java Script Recall/assets/scripts/utils.js
@@ -1,8 +1,8 @@
-export let project_manager = "Aryan";
+export let projectManager = "Aryan";
 //The export keywords let it export outside the file
 
 export default "React"
 //Using the default keyword means that this value here, should be the default thing that's exported by this file.
 //And you can have only one default export per file.
 
-export let project_technology = "JS";
+export let projectTechnology = "JS";
